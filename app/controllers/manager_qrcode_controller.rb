e46ed9c393d7e1b3087c74283ac0c2c29699fd42
mu-shock <mu-shock@example.com>
2022-06-15 23:04:40 +0900

    QRコードの表示調整

--- app/controllers/manager_qrcode_controller.rb
+++ app/controllers/manager_qrcode_controller.rb
@@ -9,7 +9,7 @@ class ManagerQrcodeController < ApplicationController
       return
     end
     qr = Rails.cache.fetch("manager_qrcode/#{text}", expired_in: 6.hours) do
-      RQRCode::QRCode.new(text).as_svg(offset: 5,module_size: 5).html_safe
+      RQRCode::QRCode.new(text).as_svg(offset: 4,module_size: 4).html_safe
     end
     render(body: qr,content_type: 'image/svg+xml')
   end
